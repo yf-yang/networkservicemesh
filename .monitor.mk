9961ea8e4a80c43781af975f6111660ef1a2688d
yf-yang <directoryyf@gmail.com>
2018-11-20 21:25:44 -0800

    signoff
    Signed-off-by: yf-yang <directoryyf@gmail.com>

--- .monitor.mk
+++ .monitor.mk
@@ -3,8 +3,8 @@ mon: mon-server mon-client
 
 .PHONY: mon-server
 mon-server:
-	CGO_ENABLED=0 GOOS=linux go build -ldflags '-extldflags "-static"' -o $(GOPATH)/bin/mon-server ./examples/skydive/monitor-server.go
+	CGO_ENABLED=0 GOOS=linux go build -ldflags '-extldflags "-static"' -o $(GOPATH)/bin/mon-server ./examples/skydive/server/monitor-server.go
 
 .PHONY: mon-client
 mon-client:
-	CGO_ENABLED=0 GOOS=linux go build -ldflags '-extldflags "-static"' -o $(GOPATH)/bin/mon-client ./examples/skydive/monitor-client.go
+	CGO_ENABLED=0 GOOS=linux go build -ldflags '-extldflags "-static"' -o $(GOPATH)/bin/mon-client ./examples/skydive/client/monitor-client.go
